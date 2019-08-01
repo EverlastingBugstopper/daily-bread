f5156d8989cf12dd631653ac5b6ceaae72c24d8f
Avery Harnish <averyharnish@gmail.com>
2019-08-01 14:13:19 -0500

    Write note input to file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 extern crate clap;
-use clap::App;
+use clap::{App, Arg, SubCommand};
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::Error;
 
-fn main() {
-    App::new("daily-bread")
+fn main() -> Result<(), Error> {
+    let matches = App::new("daily-bread")
         .version("0.1.0")
         .about("Emails your notes and commit messages to you at the end of the day")
         .author("Avery Harnish")
+        .subcommand(
+            SubCommand::with_name("note")
+                .about("Appends a note to your daily note file")
+                .arg(
+                    Arg::with_name("input")
+                        .help("the note to take")
+                        .index(1)
+                        .required(true),
+                ),
+        )
+        .subcommand(SubCommand::with_name("config").help("Sets your configuration"))
         .get_matches();
+    if let Some(matches) = matches.subcommand_matches("note") {
+        let note = matches.value_of("input").unwrap();
+        let path = "./notes/date.txt";
+        // TODO make directory if doesn't exist
+        let mut file = OpenOptions::new().create(true).append(true).open(path)?;
+        write!(file, "{}\n", note)?;
+    } else if let Some(_) = matches.subcommand_matches("config") {
+        println!("Config command run!");
+    }
+    Ok(())
 }
