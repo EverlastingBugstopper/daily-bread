fde5b9850255f58bea519d56f92fdb652e04bdda
Avery Harnish <averyharnish@gmail.com>
2019-08-01 15:13:24 -0500

    Write notes to config directory

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ use clap::{App, Arg, SubCommand};
 extern crate chrono;
 use chrono::{Datelike, Local};
 
+use directories::ProjectDirs;
+
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::Error;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Error> {
     let matches = App::new("daily-bread")
@@ -27,18 +30,22 @@ fn main() -> Result<(), Error> {
         .subcommand(SubCommand::with_name("config").help("Sets your configuration"))
         .get_matches();
     if let Some(matches) = matches.subcommand_matches("note") {
+        let note = matches.value_of("input").unwrap();
         let now = Local::now();
         let year_name = now.year();
         let year = now.format("%y");
         let month = format!("{:02}", now.month());
         let month_name = now.format("%B");
         let day = format!("{:02}", now.day());
-        let dir = format!("./notes/{}/{}", year_name, month_name);
-        let path = format!("{}/{}-{}-{}.txt", dir, month, day, year);
-        fs::create_dir_all(dir)?;
-        let note = matches.value_of("input").unwrap();
-        let mut file = OpenOptions::new().create(true).append(true).open(path)?;
-        write!(file, "{}\n", note)?;
+        if let Some(proj_dirs) = ProjectDirs::from("com", "EverlastingBugstopper", "daily-bread") {
+            let mut dir = PathBuf::from(proj_dirs.config_dir());
+            dir.push(format!("{}/{}", year_name, month_name));
+            let mut path = PathBuf::from(dir.clone());
+            path.push(format!("{}-{}-{}.txt", month, day, year));
+            fs::create_dir_all(dir)?;
+            let mut file = OpenOptions::new().create(true).append(true).open(path)?;
+            write!(file, "{}\n", note)?;
+        }
     } else if let Some(_) = matches.subcommand_matches("config") {
         println!("Config command run!");
     }
